d18421ba26fe124c6c2ee55c2bd55f1fbb1ab25e
xgtz <15809207816@163.com>
2017-10-17 00:36:28 +0800

    提交view

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Post
 
 # Create your views here.
+def homepate(request):
+	posts=Post.objects.all()
+	post_lists=list()
+	for index,post in enumerate(posts):
+		post_lists.append("No.".format(str(index)) + str(post) +"<br>") 
+	
+	return HttpResponse(post_lists)
